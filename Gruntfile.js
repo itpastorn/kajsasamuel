52b4bf211d8497e88a203522c97f77de72255019
Lars Gunther <gunther@keryx.se>
2015-05-05 18:35:10 +0200

    starting again2

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    // task setup 
+    grunt.registerTask('default', []);
+};
