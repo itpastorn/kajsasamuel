07992630a755348a34418eb72aabd8476de5aebc
Lars Gunther <gunther@keryx.se>
2015-05-26 09:09:08 +0200

    Added page vardar, fixed rewrites, going live - 2 pages

--- scripts.js
+++ scripts.js
@@ -1,32 +1,34 @@
 (function () {
-    var edays  = document.getElementById("days"),
-        ehours = document.getElementById("hours"),
-        emins  = document.getElementById("minutes"),
-        esecs  = document.getElementById("seconds"),
-        ednoun = document.getElementById("daynoun"),
-        ehnoun = document.getElementById("hournoun"),
-        emnoun = document.getElementById("minnoun"),
-        esnoun = document.getElementById("secnoun");
+    if ( document.getElementById("days") ) {
+        var edays  = document.getElementById("days"),
+            ehours = document.getElementById("hours"),
+            emins  = document.getElementById("minutes"),
+            esecs  = document.getElementById("seconds"),
+            ednoun = document.getElementById("daynoun"),
+            ehnoun = document.getElementById("hournoun"),
+            emnoun = document.getElementById("minnoun"),
+            esnoun = document.getElementById("secnoun");
 
-    function countDown() {
-        var seconds = Math.round((Date.UTC(2015,7,15,12) - Date.now())/1000);
-        var days = Math.floor(seconds/3600/24);
-        seconds -= days * 24 * 3600;
-        var hours = Math.floor(seconds/3600);
-        seconds -= hours * 3600;
-        var minutes = Math.floor(seconds/60);
-        seconds -= minutes * 60;
-        edays.innerHTML  = days;
-        ehours.innerHTML = hours;
-        emins.innerHTML  = minutes;
-        esecs.innerHTML  = seconds;
-        ednoun.innerHTML = (Math.abs(days) === 1) ? "": "ar";
-        ehnoun.innerHTML = (Math.abs(hours) === 1) ? "e": "ar";
-        emnoun.innerHTML = (Math.abs(minutes) === 1) ? "": "er";
-        esnoun.innerHTML = (Math.abs(seconds) === 1) ? "": "er";
+        function countDown() {
+            var seconds = Math.round((Date.UTC(2015,7,15,12) - Date.now())/1000);
+            var days = Math.floor(seconds/3600/24);
+            seconds -= days * 24 * 3600;
+            var hours = Math.floor(seconds/3600);
+            seconds -= hours * 3600;
+            var minutes = Math.floor(seconds/60);
+            seconds -= minutes * 60;
+            edays.innerHTML  = days;
+            ehours.innerHTML = hours;
+            emins.innerHTML  = minutes;
+            esecs.innerHTML  = seconds;
+            ednoun.innerHTML = (Math.abs(days) === 1) ? "": "ar";
+            ehnoun.innerHTML = (Math.abs(hours) === 1) ? "e": "ar";
+            emnoun.innerHTML = (Math.abs(minutes) === 1) ? "": "er";
+            esnoun.innerHTML = (Math.abs(seconds) === 1) ? "": "er";
+        }
+        countDown();
+        setInterval(countDown, 200);
     }
-    countDown();
-    setInterval(countDown, 200);
 
     function widthChange(mq) {
         var lbl = document.getElementsByTagName("label")[0];
